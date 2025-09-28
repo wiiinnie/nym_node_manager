97a65f7f8e950d4a5268fef62296e3e4294151f7
Matthias Winter <winnie_@gmx.net>
2025-09-28 14:39:58 +0200

    This is only a test for the update workflow

--- nym_node_manager.sh
+++ nym_node_manager.sh
@@ -2,6 +2,7 @@
 
 # Nym Node Manager v40 - Optimized Version
 # Requires: dialog, sshpass, curl
+# This is only a test for GIT workflow
 
 # Colors and config
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; NC='\033[0m'
